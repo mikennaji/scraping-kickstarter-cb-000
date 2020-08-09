70feb22bd0ee7974996d144acea26b38f2e5ed08
Michael Nnaji <mnnaji3@uwo.ca>
2020-08-09 23:51:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -20,7 +20,7 @@ def create_project_hash
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-    
+
     }
   end
   puts projects
