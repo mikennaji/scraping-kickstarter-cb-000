ad3c7f0a78b75ab95a7e1cb87686c7f7ace1627a
Michael Nnaji <mnnaji3@uwo.ca>
2020-08-09 23:22:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,16 @@ require 'pry'
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  projects = {}
+  kickstarter.css("li.project.grid_4").each do |project|
+   projects[project] = {}
+  end 
+  projects
 
   # return the projects hash
  
 end
+
+create_project_hash
